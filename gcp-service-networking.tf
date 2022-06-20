605970fd6e40539e254bae76830331df062e2a51
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-06-20 20:56:58 +0000

    feat: adding back gcp private connect network

--- gcp-service-networking.tf
+++ gcp-service-networking.tf
@@ -1,7 +1,17 @@
-# resource "google_service_networking_connection" "foobar" {
-#   network                 = google_compute_network.peering_network.id
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
-# }
+resource "google_compute_global_address" "gcp_managed_services" {
+  name          = "${var.workspace}-gcp-services-network"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  addresses     = split("/", var.network_prefixes["kubernetes_services"])[0]
+  prefix_length = split("/", var.network_prefixes["kubernetes_services"])[1]
+  network       = google_compute_network.vpc_network.id
+}
+
+
+resource "google_service_networking_connection" "foobar" {
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.gcp_managed_services.name]
+}
 
 
