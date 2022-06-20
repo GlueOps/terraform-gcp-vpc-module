27c481517b4f88c91c6dc318969ac6332dddc118
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-06-20 22:27:21 +0000

    fixes

--- vpc-access-connector.tf
+++ vpc-access-connector.tf
@@ -1,6 +1,6 @@
 resource "google_vpc_access_connector" "connector" {
   project       = data.google_projects.env_project.projects[0].project_id
-  name          = "vpc-serverless-access-connection"
+  name          = "vpc-access-connector"
   ip_cidr_range = var.network_prefixes["serverless_vpc_connector"]
   network       = google_compute_network.vpc_network.name
   region        = var.region
