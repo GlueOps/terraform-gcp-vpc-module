852ec7d6dcfa3078e460be04b349feccbf5df392
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-06-20 20:58:03 +0000

    fixes

--- gcp-service-networking.tf
+++ gcp-service-networking.tf
@@ -2,7 +2,7 @@ resource "google_compute_global_address" "gcp_managed_services" {
   name          = "${var.workspace}-gcp-services-network"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
-  addresses     = split("/", var.network_prefixes["kubernetes_services"])[0]
+  address       = split("/", var.network_prefixes["kubernetes_services"])[0]
   prefix_length = split("/", var.network_prefixes["kubernetes_services"])[1]
   network       = google_compute_network.vpc_network.id
 }
