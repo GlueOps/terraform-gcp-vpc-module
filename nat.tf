1744fa48457ced8f32aed6027f04a2b033918ec1
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-06-23 12:05:18 -0700

    Update NAT to tunnel all subnetworks and all cidrs

--- nat.tf
+++ nat.tf
@@ -14,12 +14,7 @@ resource "google_compute_router_nat" "nat" {
   name                               = "${var.workspace}-${var.region}-nat"
   router                             = google_compute_router.router.name
   region                             = google_compute_router.router.region
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-
-  subnetwork {
-    name                    = google_compute_subnetwork.private-network.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
   nat_ip_allocate_option = "AUTO_ONLY"
 
